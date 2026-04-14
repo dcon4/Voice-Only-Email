03966600453a2c0becd5a8947a4a9f3c95b3382f
dcon4 <105887474+dcon4@users.noreply.github.com>
2026-04-13 19:23:53 -0500

    Update MainActivity.kt with basic Kotlin Android activity implementation

---
+++ MainActivity.kt
@@ -0,0 +1,14 @@
+package com.voiceemail
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // TODO: Wire up the root Compose UI here
+        }
+    }
+}
