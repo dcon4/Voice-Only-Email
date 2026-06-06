544392237d0e610a5ca725a18b535c20e4d3ad4f
dcon4 <dcon2000@gmail.com>
2026-06-06 16:38:02 -0500

    Change default name from 'Unknown Name' to 'Unknown'

--- app/src/main/java/com/example/voicegmail/contacts/Contact.java
+++ app/src/main/java/com/example/voicegmail/contacts/Contact.java
@@ -5,7 +5,7 @@ public class Contact {
     private final String email;
 
     public Contact(String name, String email) {
-        this.name = (name != null) ? name : "Unknown Name";
+        this.name = (name != null) ? name : "Unknown";
         this.email = (email != null) ? email : "";
     }
 
