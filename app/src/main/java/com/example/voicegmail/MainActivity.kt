f2fd7e2c439503bb67361c2e5595054f6fabec3a
dcon4 <dcon2000@gmail.com>
2026-06-08 16:38:53 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/voicegmail/MainActivity.kt
+++ app/src/main/java/com/example/voicegmail/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.voicegmail.contacts.ContactManager
